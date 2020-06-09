0e6af3b28de2f4db6e38e93bdc0180879ffd19a1
abc414 <66683563+abc414@users.noreply.github.com>
2020-06-09 09:36:50 -0500

    Initial commit

---
+++ hello.c(1).c
@@ -0,0 +1,15 @@
+/**
+* Author: Nick and Griffin
+*
+* Date: 2020/06/08
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+	printf("Hello World\n");
+	
+	return 0;
+}
